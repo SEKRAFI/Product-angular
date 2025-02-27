3d21232a955a2ffdd1f73df78a9c913e8d321569
Ismail.SEKRAFI <ismail.sekrafi@scalian.com>
2025-02-27 11:54:50 +0100

    Déploiement de l'application Angular en production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageDisplayComponent } from './image-display/image-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ImageDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/image-display/image-display.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-display',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
+  templateUrl: './image-display.component.html',
+  styleUrls: ['./image-display.component.css']
+})
+export class ImageDisplayComponent {
+  selectedImages: string[] = []; // Tableau pour les images affichées quand un lien est sélectionné
+  selectedProduct: string = '';  // Variable pour stocker le titre du produit sélectionné
+  linksAndImages: any[] = []; // Tableau qui contiendra les produits et leurs images
+
+  constructor(private http: HttpClient) {}
+
+  // Fonction de chargement des données à partir du fichier JSON
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  // Chargement des produits depuis le fichier JSON
+  loadProducts(): void {
+    this.http.get<any>('assets/products.json').subscribe(data => {
+      this.linksAndImages = data.produits;
+    }, error => {
+      console.error('Erreur lors du chargement des produits:', error);
+    });
+  }
+
+  // Fonction appelée lorsqu'un lien est cliqué pour afficher les images
+  displayImages(link: string): void {
+    const selectedLink = this.linksAndImages.find(l => l.title === link);
+    if (selectedLink) {
+      this.selectedProduct = selectedLink.title; // Met à jour le titre du produit sélectionné
+      this.selectedImages = selectedLink.images; // Met à jour les images du produit sélectionné
+    }
+  }
+}
